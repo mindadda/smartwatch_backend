1d8b9faeb1268da45567c970f31b583c370db397
poojadas07 <poojadas04kv@gmail.com>
2021-03-07 13:02:44 +0530

    Panel added

--- src/main/java/com/htlabs/smartwatch/service/impl/PanelServiceImpl.java
+++ src/main/java/com/htlabs/smartwatch/service/impl/PanelServiceImpl.java
@@ -1,27 +1,50 @@
 package com.htlabs.smartwatch.service.impl;
 
 import com.htlabs.smartwatch.dto.PanelDTO;
+import com.htlabs.smartwatch.entity.Department;
 import com.htlabs.smartwatch.entity.Panel;
+import com.htlabs.smartwatch.entity.ScreenDetails;
 import com.htlabs.smartwatch.entity.converter.PanelConverter;
+import com.htlabs.smartwatch.entity.converter.ScreenConverter;
 import com.htlabs.smartwatch.repository.PanelRepository;
-import com.htlabs.smartwatch.service.IotService;
+import com.htlabs.smartwatch.repository.ScreenRepository;
+import com.htlabs.smartwatch.service.PanelService;
+import com.htlabs.smartwatch.utils.ErrorMessages;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 @Slf4j
-public class IotServiceImpl implements IotService {
+public class PanelServiceImpl implements PanelService {
 
     @Autowired
     private PanelRepository panelRepository;
+
+    @Autowired
+    private ScreenRepository screenRepository;
 
     @Override
     public List<PanelDTO> getPanelValue() {
         log.info("Retrieving the PanelValue.");
         List<Panel> panels = panelRepository.findAll();
+        return PanelConverter.getPanelDTOListFromEntityList(panels);
+    }
+
+    @Override
+    public List<PanelDTO> getPanelsByScreen(String screenName) {
+        log.info("Retrieving Panel by screen ");
+        String screenId = screenRepository.findScreenName(screenName);
+        ScreenDetails screenDetails = screenRepository.findById(screenId).orElse(null);
+        if(screenDetails == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessages.INVALID_SCREEN);
+        }
+
+        List<Panel> panels = panelRepository.findByScreenId(screenId);
         return PanelConverter.getPanelDTOListFromEntityList(panels);
     }
 }
