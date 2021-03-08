d1932c1e79a3283d69593167b74eb5079b1792ca
poojadas07 <poojadas04kv@gmail.com>
2021-03-08 13:04:23 +0530

    Panel service

--- src/main/java/com/htlabs/smartwatch/service/PanelService.java
+++ src/main/java/com/htlabs/smartwatch/service/PanelService.java
@@ -4,7 +4,9 @@ import com.htlabs.smartwatch.dto.PanelDTO;
 
 import java.util.List;
 
-public interface IotService {
+public interface PanelService {
 
     public List<PanelDTO> getPanelValue();
+
+    public List<PanelDTO> getPanelsByScreen(String screenName);
 }
