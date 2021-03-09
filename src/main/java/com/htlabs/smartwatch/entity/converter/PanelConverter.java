5df913a1b17ca45ee948ef789bd806a2de9f5b20
poojadas07 <poojadas04kv@gmail.com>
2021-03-09 13:06:19 +0530

    Panel converter

--- src/main/java/com/htlabs/smartwatch/entity/converter/PanelConverter.java
+++ src/main/java/com/htlabs/smartwatch/entity/converter/PanelConverter.java
@@ -1,2 +1,22 @@
-package com.htlabs.smartwatch.entity.converter;public class PanelConverter {
+package com.htlabs.smartwatch.entity.converter;
+
+import com.google.common.reflect.TypeToken;
+import com.htlabs.smartwatch.dto.PanelDTO;
+import com.htlabs.smartwatch.entity.Panel;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class PanelConverter {
+
+    private PanelConverter() {
+
+    }
+
+
+    public static List<PanelDTO> getPanelDTOListFromEntityList(List<Panel> panels) {
+        return new ModelMapper().map(panels, new TypeToken<List<PanelDTO>>() {
+        }.getType());
+    }
 }
