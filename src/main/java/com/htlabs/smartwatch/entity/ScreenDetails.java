c7d040594dcd685e83b24d88ee3094c2225dc3d9
poojadas07 <poojadas04kv@gmail.com>
2021-03-09 13:06:40 +0530

    Screen details

--- src/main/java/com/htlabs/smartwatch/entity/ScreenDetails.java
+++ src/main/java/com/htlabs/smartwatch/entity/ScreenDetails.java
@@ -18,14 +18,22 @@ public class ScreenDetails extends AuditEntity{
     @Column(name = "screen_name")
     private String screenName;
 
+    @Column(name = "row_count")
+    private Integer rowCount;
+
+    @Column(name = "column_count")
+    private Integer columnCount;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id")
     private Department department;
 
-    public ScreenDetails(String screenId , String screenName){
+    public ScreenDetails(String screenId , String screenName , Integer rowCount , Integer columnCount){
         super();
         this.screenId = screenId;
         this.screenName = screenName;
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
     }
 
     public ScreenDetails(){
