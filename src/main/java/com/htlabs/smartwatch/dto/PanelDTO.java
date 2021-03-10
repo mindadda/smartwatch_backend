63e36af5b1c2a30f0da60aa7f26957b62232596b
poojadas07 <poojadas04kv@gmail.com>
2021-03-10 13:10:29 +0530

    Panel dto

--- src/main/java/com/htlabs/smartwatch/dto/PanelDTO.java
+++ src/main/java/com/htlabs/smartwatch/dto/PanelDTO.java
@@ -1,2 +1,42 @@
-package com.htlabs.smartwatch.dto;public class PanelDTO {
+package com.htlabs.smartwatch.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PanelDTO {
+
+    private String panelId;
+
+    private String panelName;
+
+    private Integer rowNo;
+
+    private Integer columnNo;
+
+    private String currentValue;
+
+    private String currentTime;
+
+    private String currentUpdatedTime;
+
+    private String previousValue;
+
+    private String previousTime;
+
+    private String previousUpdatedTime;
+
+    private String sensorId;
+
+    private ScreenDTO screen;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
 }
