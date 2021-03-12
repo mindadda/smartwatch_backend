9d3280f79ac7a8b191bfc65b9c9af4f627f75d35
poojadas07 <poojadas04kv@gmail.com>
2021-03-12 11:31:48 +0530

    Topic changes

--- src/main/java/com/htlabs/smartwatch/Constants.java
+++ src/main/java/com/htlabs/smartwatch/Constants.java
@@ -2,9 +2,9 @@ package com.htlabs.smartwatch;
 
 public final class Constants {
 
-	public static final String WEBSOCKET_DESTINATION = "/topic/messages";
+	public static final String WEBSOCKET_DESTINATION = "/topic/topic";
 
-	public static final String KAFKA_TOPIC = "messages";
+	public static final String KAFKA_TOPIC = "topic";
 
 	private Constants() {
 
